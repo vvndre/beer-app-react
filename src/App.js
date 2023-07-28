38502692506e465970d5a72d61dffec12898ac54
andrercastillon <andrercastillon@gmail.com>
2023-07-28 13:17:53 -0500

    react comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beers: [],
+    };
+  }
+  componentDidMount() {
+    axios.get("https://api.punkapi.com/v2/beers").then(({ data }) => {
+      this.setState({ beers: [] });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.beers.map((beer, index) => {
+          return <Beer key={beer.id} beer={beer}></Beer>;
+        })}
+      </div>
+    );
+  }
 }
 
-export default App;
+class Beer extends React.Component {
+  render() {
+    return <div>{/* this.prop.beer */}</div>;
+  }
+}
